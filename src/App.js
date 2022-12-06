175c9bc17c63e00989d4abd67e6913ef95752cbf
hiepphh <hoanghiepphambk@gmail.com>
2022-12-06 15:30:28 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import SearchBar from './components/search-bar/search-bar.component';
+import CardList from './components/card-list/card-list.component';
 
-function App() {
+const App = () => {
+  const [cardList , setCardList] = useState([]);
+  const [searchField , setSearchField] = useState('');
+  const [filterList , setFilterList] = useState(cardList);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        console.log('1')
+        return response.json()
+      })
+      .then((users) => {
+        console.log('2');
+        setCardList(users)
+      })
+  }, []);
+
+  useEffect(() => {
+    console.log('4', cardList)
+    const filterList = cardList.filter((card) => {
+      return card.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilterList(filterList);
+  },[cardList, searchField])
+
+  const searchFunc = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    setSearchField(searchField);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List card</h1>
+      <SearchBar searchFunc={searchFunc} placeholder="Type to search"></SearchBar>
+      <CardList cardList={filterList}></CardList>
     </div>
   );
 }
